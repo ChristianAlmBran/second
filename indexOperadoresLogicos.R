7c993c2c750da13a22b19272e5c428fc0c95fe92
ChristiamAlmBran <chrisvonbran@gmail.com>
2022-08-21 20:56:24 -0300

    Exercício index e operadores lógicos

---
+++ indexOperadoresLogicos.R
@@ -0,0 +1,12 @@
+# criando dataframe
+familyNamesAges <- data.frame (name = c("John", "Peter", "Ana"), 
+                               age = c(45, 28, 12))
+
+#recuperando os nomes
+familyNamesAges$name
+
+#recuperando as idades
+familyNamesAges[ , 2]
+
+#recuperando as duas primeiras linhas
+familyNamesAges[1:2, ]
