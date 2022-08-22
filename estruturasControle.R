98cabafeb77e72a8b43683852d6cba4d0c54cfae
ChristiamAlmBran <chrisvonbran@gmail.com>
2022-08-21 22:20:15 -0300

    Exercício estruturas de controle. Versão 2, comentarios adicionados

--- estruturasControle.R
+++ estruturasControle.R
@@ -1,16 +1,20 @@
+# criando as variaveis
 a <- 1
 b <- 2
 c <- 3
 
+# criando o loop no padrão if/else
 if (a > c) {
   print("a must be bigger than b")
 } else {
   print("a must be smaller than b")
 }
 
+# criando o loop no padrão for
 for (value in iris$Species) {
   print(value)
 }
 
+# utilizando o lapply 
 lapply(iris[, 1:3], hist)
 
