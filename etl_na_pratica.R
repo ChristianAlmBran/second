889455a658fa0ab6e7a31cbf7174c498a027392d
ChristiamAlmBran <chrisvonbran@gmail.com>
2022-09-05 10:33:17 -0300

    Exercício ETL na prática. Versão 2

--- etl_na_pratica.R
+++ etl_na_pratica.R
@@ -1,17 +1,25 @@
 pacman::p_load(dplyr)
 pacman::p_load(readr)
 
+# carrega a base de sinistros de transito do site da PCR
 sinistrosRecife2019Raw <- read.csv2('http://dados.recife.pe.gov.br/dataset/44087d2d-73b5-4ab3-9bd8-78da7436eed1/resource/3531bafe-d47d-415e-b154-a881081ac76c/download/acidentes-2019.csv', sep = ';', encoding = 'UTF-8')
 
-sinistrosRecife2019Raw <- sinistrosRecife2019Raw %>%
-  rename(data = DATA)
-
-# carrega a base de snistros de transito do site da PCR
 sinistrosRecife2020Raw <- read.csv2('http://dados.recife.pe.gov.br/dataset/44087d2d-73b5-4ab3-9bd8-78da7436eed1/resource/fc1c8460-0406-4fff-b51a-e79205d1f1ab/download/acidentes_2020-novo.csv', sep = ';', encoding = 'UTF-8')
 
-sinistrosRecife2021Raw <- read.csv2('http://dados.recife.pe.gov.br/dataset/44087d2d-73b5-4ab3-9bd8-78da7436eed1/resource/2caa8f41-ccd9-4ea5-906d-f66017d6e107/download/acidentes_2021-jan.csv', sep = ';', encoding = 'UTF-8')
+sinistrosRecife2021Raw <- read.csv2('http://dados.recife.pe.gov.br/dataset/44087d2d-73b5-4ab3-9bd8-78da7436eed1/resource/2caa8f41-ccd9-4ea5-906d-f66017d6e107/download/acidentes_2021-jan.csv', sep = ';', encoding = 'UTF-8') 
+
+# transformando a coluna DATA do base de dados do ano 2019
+names(sinistrosRecife2019Raw)[names(sinistrosRecife2019Raw) == 'DATA'] <- 'data'
 
 # junta as bases de dados com comando rbind (juntas por linhas)
+colunas_iguais_2 <- names(sinistrosRecifeRaw[
+  intersect(
+    names(sinistrosRecifeRaw), names(sinistrosRecife2019Raw))])
+
+sinistrosRecife2019Raw <- sinistrosRecife2019Raw %>% select(all_of(colunas_iguais))
+
+sinistrosRecifeRaw_2 <- rbind(sinistrosRecife2019Raw, sinistrosRecifeRaw)
+
 colunas_iguais <- names(sinistrosRecife2020Raw[
   intersect(
     names(sinistrosRecife2020Raw), names(sinistrosRecife2021Raw))])
@@ -21,13 +29,17 @@ sinistrosRecife2020Raw <- sinistrosRecife2020Raw %>% select(all_of(colunas_iguai
 sinistrosRecifeRaw <- rbind(sinistrosRecife2020Raw, sinistrosRecife2021Raw)
 
 # observa a estrutura dos dados
-str(sinistrosRecifeRaw)
+str(sinistrosRecifeRaw_2)
 
 # modifca a data para formato date
-sinistrosRecifeRaw$data <- as.Date(sinistrosRecifeRaw$data, format = "%Y-%m-%d")
+sinistrosRecifeRaw_2$data <- as.Date(sinistrosRecifeRaw_2$data, format = "%Y-%m-%d")
 
 # modifica natureza do sinistro de texto para fator
-sinistrosRecifeRaw$natureza_acidente <- as.factor(sinistrosRecifeRaw$natureza_acidente)
+sinistrosRecifeRaw_2$natureza_acidente <- as.factor(sinistrosRecifeRaw_2$natureza_acidente)
+
+#modifica condição da via de texto para fator
+sinistrosRecifeRaw_2$condicao_via <- as.factor
+(sinistrosRecifeRaw_2$condicao_via)
 
 # cria funçaõ para substituir not available (na) por 0
 naZero <- function(x) {
