057b554a81b896a11069b1d53cb33af2a662e541
ChristianAlmBran <111160878+ChristianAlmBran@users.noreply.github.com>
2022-09-05 17:31:39 -0300

    Update tiposDadosR.R

--- tiposDadosR.R
+++ tiposDadosR.R
@@ -1,5 +1,5 @@
 # criando fator
 nacionalidade <- c(1, 2, 0, 1, 2, 2, 2, 2, 1, 0, 1, 1, 1, 1)
-recode <- c(brasileiro = 1, extrangeiro = 2)
+recode <- c(brasileiro = 1, estrangeiro = 2)
 nacionalidade <- factor(nacionalidade, levels = recode, labels = names(recode))
 view(nacionalidade)
